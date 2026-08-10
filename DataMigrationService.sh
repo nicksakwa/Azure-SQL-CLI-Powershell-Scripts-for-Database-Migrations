d4acbb406787b913708fb28de85ec68c745130be
nicksakwa <nsakwa@gmail.com>
2026-08-10 14:57:22 +0300

    trailing spaces & missing slashes fixed

--- DataMigrationService.sh
+++ DataMigrationService.sh
@@ -3,8 +3,8 @@ az extension add --name datamigration
 
 # Create the Azure DMS service
 az datamigration sql-service create --resource-group "<YourResourceGroup>" \
-    --sql-migration-service-name "<YourMigrationService>" \  
-    --location "<YourLocation>"                            
+    --sql-migration-service-name "<YourMigrationService>" \
+    --location "<YourLocation>"
 
 # Migrate schema from source to target database
 az datamigration sql-server-schema \
@@ -14,18 +14,18 @@ az datamigration sql-server-schema \
 
 # Create a database migration to Azure SQL Database
 az datamigration sql-db create \
-    --resource-group "<YourResourceGroup>" \            
-    --sqldb-instance-name "<YourTargetServer>" \        
-    --target-db-name "<YourTargetDB>" \                 
-    --source-database-name "<YourSourceDB>" \           
-    --source-sql-connection authentication="SqlAuthentication" 
-        data-source="<YourSourceServer>"\              
-        user-name="<YourSourceUser>" \                  
-        password="<YourSourcePassword>" \               
+    --resource-group "<YourResourceGroup>" \
+    --sqldb-instance-name "<YourTargetServer>" \
+    --target-db-name "<YourTargetDB>" \
+    --source-database-name "<YourSourceDB>" \
+    --source-sql-connection authentication="SqlAuthentication" \
+        data-source="<YourSourceServer>" \
+        user-name="<YourSourceUser>" \
+        password="<YourSourcePassword>" \
         encrypt-connection=true trust-server-certificate=true \
-    --target-sql-connection authentication="SqlAuthentication" data-source="<YourTargetServer>.database.windows.net" \  
-        user-name="<YourTargetUser>" \                  
-        password="<YourTargetPassword>" \               
-        encrypt-connection=true trust-server-certificate=true 
-    --scope "/subscriptions/<YourSubscription>/resourceGroups/<YourResourceGroup>/providers/Microsoft.Sql/servers/<YourTargetServer>" 
+    --target-sql-connection authentication="SqlAuthentication" data-source="<YourTargetServer>.database.windows.net" \
+        user-name="<YourTargetUser>" \
+        password="<YourTargetPassword>" \
+        encrypt-connection=true trust-server-certificate=true \
+    --scope "/subscriptions/<YourSubscription>/resourceGroups/<YourResourceGroup>/providers/Microsoft.Sql/servers/<YourTargetServer>" \
     --migration-service "/subscriptions/<YourSubscription>/resourceGroups/<YourResourceGroup>/providers/Microsoft.DataMigration/sqlMigrationServices/<YourMigrationService>"

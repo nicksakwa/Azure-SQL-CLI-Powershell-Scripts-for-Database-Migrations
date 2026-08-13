13bbefb16919061d2903464602869b9361d4c221
nicksakwa <nsakwa@gmail.com>
2026-08-13 23:04:26 +0300

    ps1 extension update & DMS.ps1 syntax fix

---
+++ DataMigrationService.ps1
@@ -0,0 +1,21 @@
+# Set up secure credentials for source and target connections
+$sourcePass = ConvertTo-SecureString "<YourSourcePassword>" -AsPlainText -Force
+$targetPass = ConvertTo-SecureString "<YourTargetPassword>" -AsPlainText -Force
+
+# Start the database migration to Azure SQL Database
+New-AzDataMigrationToSqlDb `
+    -ResourceGroupName "<YourResourceGroup>" `
+    -SqlDbInstanceName "<YourTargetServer>" `
+    -Kind "SqlDb" `
+    -TargetDbName "<YourTargetDB>" `
+    -SourceDatabaseName "<YourSourceDB>" `
+    -SourceSqlConnectionAuthentication SQLAuthentication `
+    -SourceSqlConnectionDataSource "<YourSourceServer>" `
+    -SourceSqlConnectionUserName "<YourSourceUser>" `
+    -SourceSqlConnectionPassword $sourcePass `
+    -Scope "/subscriptions/<YourSubscription>/resourceGroups/<YourResourceGroup>/providers/Microsoft.Sql/servers/<YourTargetServer>" `
+    -TargetSqlConnectionAuthentication SQLAuthentication `
+    -TargetSqlConnectionDataSource "<YourTargetServer>.database.windows.net" `
+    -TargetSqlConnectionUserName "<YourTargetUser>" `
+    -TargetSqlConnectionPassword $targetPass `
+    -MigrationService "/subscriptions/<YourSubscription>/resourceGroups/<YourResourceGroup>/providers/Microsoft.DataMigration/SqlMigrationServices/<YourMigrationService>"

a1cdd497b62cb8ae98e4f156829d5d13c7cefbaf
nicksakwa <nsakwa@gmail.com>
2026-08-13 23:08:12 +0300

    DMS migrate specific tables ps sripts

--- DataMigrationService.ps1
+++ DataMigrationService.ps1
@@ -19,3 +19,22 @@ New-AzDataMigrationToSqlDb `
     -TargetSqlConnectionUserName "<YourTargetUser>" `
     -TargetSqlConnectionPassword $targetPass `
     -MigrationService "/subscriptions/<YourSubscription>/resourceGroups/<YourResourceGroup>/providers/Microsoft.DataMigration/SqlMigrationServices/<YourMigrationService>"
+
+# Migrate specific tables from source to target database
+New-AzDataMigrationToSqlDb `
+    -ResourceGroupName "<YourResourceGroup>" `
+    -SqlDbInstanceName "<YourTargetServer>" `
+    -Kind "SqlDb" `
+    -TargetDbName "<YourTargetDB>" `
+    -SourceDatabaseName "<YourSourceDB>" `
+    -SourceSqlConnectionAuthentication SQLAuthentication `
+    -SourceSqlConnectionDataSource "<YourSourceServer>" `
+    -SourceSqlConnectionUserName "<YourSourceUser>" `
+    -SourceSqlConnectionPassword $sourcePass `
+    -Scope "/subscriptions/<YourSubscription>/resourceGroups/<YourResourceGroup>/providers/Microsoft.Sql/servers/<YourTargetServer>" `
+    -TargetSqlConnectionAuthentication SQLAuthentication `
+    -TargetSqlConnectionDataSource "<YourTargetServer>.database.windows.net" `
+    -TargetSqlConnectionUserName "<YourTargetUser>" `
+    -TargetSqlConnectionPassword $targetPass `
+    -TableList "[Person].[Person]", "[Person].[EmailAddress]" `  # Specify tables to migrate
+    -MigrationService "/subscriptions/<YourSubscription>/resourceGroups/<YourResourceGroup>/providers/Microsoft.DataMigration/SqlMigrationServices/<YourMigrationService>"

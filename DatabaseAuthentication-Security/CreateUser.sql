6f0e3e705acef4ef787bc9c63659dd65089be0de
nicksakwa <nsakwa@gmail.com>
2026-08-17 17:44:33 +0300

    create DB roles

--- DatabaseAuthentication-Security/CreateUser.sql
+++ DatabaseAuthentication-Security/CreateUser.sql
@@ -5,12 +5,15 @@ GO
 -- create user at SQL instance level
 USE [master]
 GO
-
-CREATE LOGIN demo WITH PASSWORD ='YourPassword'
-GO
-
+CREATE LOGIN demo WITH PASSWORD ='YourPassword';
 USE [WorldWideImporters]
 GO
+CREATE USER demo FOR LOGIN demo;
 
-CREATE USER demo FOR LOGIN demo
-GO
+-- create DB roles
+CREATE USER [DP300User1] WITH PASSWORD = 'YourPassword1';
+CREATE USER [DP300User2] WITH PASSWORD = 'YourPassword2';
+CREATE ROLE [SalesReader];
+ALTER ROLE [SalesReader] ADD MEMBER [DP300User1];
+ALTER ROLE [SalesReader] ADD MEMBER [DP300User2];
+GRANT SELECT ON SCHEMA::Sales TO [SalesReader];

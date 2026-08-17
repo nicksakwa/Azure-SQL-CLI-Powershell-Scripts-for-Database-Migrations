992cc787f0c60c698dd352b754a2b8d5aed6fb3d
nicksakwa <nsakwa@gmail.com>
2026-08-17 13:53:33 +0300

    Restore DB to MI from Az Blob Storage

--- backup-restoredb.sql
+++ backup-restoredb.sql
@@ -1,3 +1,5 @@
 -- Backing up DB to Azure Blob Storage
 BACKUP DATABASE YourDatabase TO URL='https://yourstorageaccount.blob.core.windows.net/yourcontainer/YourDatabase.bak' WITH COPY_ONLY
 
+-- Restoring DB to Managed Instance from Azure Blob Storage
+RESTORE DATABASE YourDatabase FROM URL='https://yourstorageaccount.blob.core.windows.net/yourcontainer/YourDatabase.bak' 

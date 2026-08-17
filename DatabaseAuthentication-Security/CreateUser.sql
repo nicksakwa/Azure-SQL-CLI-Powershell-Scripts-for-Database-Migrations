987788ba33b5a93ea589fb49fbe6a9cbab032952
nicksakwa <nsakwa@gmail.com>
2026-08-17 14:21:36 +0300

    User at SQL instance level with master

--- DatabaseAuthentication-Security/CreateUser.sql
+++ DatabaseAuthentication-Security/CreateUser.sql
@@ -1,3 +1,16 @@
 -- create user in DB External not in master for AZ DB auth
 CREATE USER [dba@constos.com] FROM EXTERNAL PROVIDER;
 GO
+
+-- create user at SQL instance level
+USE [master]
+GO
+
+CREATE LOGIN demo WITH PASSWORD ='YourPassword'
+GO
+
+USE [WorldWideImporters]
+GO
+
+CREATE USER demo FOR LOGIN demo
+GO

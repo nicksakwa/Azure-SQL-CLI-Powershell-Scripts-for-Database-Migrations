23932fe91d74aa5336866bd5eea27ef098d96b3d
nicksakwa <nsakwa@gmail.com>
2026-08-31 15:07:48 +0300

    create master key

--- DatabaseAuthentication-Security/purview.sql
+++ DatabaseAuthentication-Security/purview.sql
@@ -1,2 +1,6 @@
+-- Create Purview account user and assign db_owner role
 CREATE user <purview-account> FROM EXTERNAL PROVIDER; GO
 EXEC sp_addrolemember 'db_owner', '<purview-account>'; GO
+
+CREATE MASTER KEY 
+GO

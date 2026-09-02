9194b2a9553a4d83d57d3cadb480f3efd76a796f
nicksakwa <nsakwa@gmail.com>
2026-09-02 19:30:55 +0300

    xevent creation

--- PerformanceMonitoring/xevents.sql
+++ PerformanceMonitoring/xevents.sql
@@ -12,3 +12,15 @@ ORDER BY
     obj.object_type,
     pkg.name,
     obj.name;
+
+IF EXISTS (SELECT * FROM sys.server_event_sessions WHERE name='test_session')
+    DROP EVENT session test_session ON SERVER;
+GO
+
+CREATE EVENT SESSION test_session
+ON SERVER
+    ADD EVENT sqlos.async_io_requested,
+    ADD EVENT sqlserver.lock_acquired
+    ADD TARGET package0.etw_classic_sync_target (SET default_etw_session_logfile_path = N'C:\demo\traces\sqletw.etl' )
+    WITH (MAX_MEMORY=4MB, MAX_EVENT_SIZE=4MB);
+GO

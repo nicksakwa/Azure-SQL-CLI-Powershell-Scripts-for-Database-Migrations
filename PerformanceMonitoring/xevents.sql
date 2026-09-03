c78d5aca0b3b7d24ea4f9f99c89de05a61cdda87
nicksakwa <nsakwa@gmail.com>
2026-09-03 14:49:56 +0300

    create event session after checking/dropping previous one

--- PerformanceMonitoring/xevents.sql
+++ PerformanceMonitoring/xevents.sql
@@ -14,8 +14,16 @@ ORDER BY
     pkg.name,
     obj.name;
 
--- Create events session but 1st check if one exists
+-- Create events session but 1st check if one exists and then drops it 
 IF EXISTS (SELECT * FROM sys.server_event_sessions WHERE name='test_session')
     DROP EVENT SESSION test_session ON SERVER;
 GO
 
+-- create events session after check
+CREATE EVENT SESSION test_session
+ON SERVER
+    ADD EVENT sqlos.async_io_requested,
+    ADD EVENT sqlserver.lock_acquired
+    ADD TARGET package0.etw_classic_sync_target (SET default_etw_session_logfile_path= N'C:\demo\traces\sqletw.etl')
+    WITH (MAX_MEMORY=4MB, MAX_EVENT_SIZE=4MB);
+GO

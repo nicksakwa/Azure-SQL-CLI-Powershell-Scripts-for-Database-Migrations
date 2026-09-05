01ffadf08beca1f527d3d28b840ab6926ac9dc07
nickspartan01 <nsakwa@gmail.com>
2026-09-05 11:20:21 +0300

    classifier function explanation

--- PerformanceMonitoring/classifierfunction.sql
+++ PerformanceMonitoring/classifierfunction.sql
@@ -1,9 +1,14 @@
+-- creates User Defined Function (UDF) with the dbo schema
 CREATE FUNCTION dbo.RGClassifier()
+
+-- Built in SQL datatype SYSNAME like nvarchar(128)
 RETURNS SYSNAME
 WITH SCHEMABINDING
 AS
 BEGIN
 DECLARE @WorkloadGroup AS SYSNAME;
+
+--check if suser login name matches Report User is the UD workload group
 IF(SUSER_NAME()='ReporterUser')
     SET @WorkloadGroup='ReporterServerGroup'
 ELSE IF (SUSER_NAME()='PrimaryUser')

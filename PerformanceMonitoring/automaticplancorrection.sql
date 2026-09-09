6a134d288d6d19e2f5d81cedb03c1720bf684809
nicksakwa <nsakwa@gmail.com>
2026-09-09 10:11:24 +0300

    automatictuning

--- PerformanceMonitoring/automaticplancorrection.sql
+++ PerformanceMonitoring/automaticplancorrection.sql
@@ -1,1 +1,9 @@
 ALTER DATABASE [WideWorldImporters] SET AUTOMATIC_TUNING (FORCE_LAST_GOOD_PLAN = ON);
+
+SELECT 
+    name,
+    desired_state_desc,
+    reason_desc
+FROM sys.database_automatic_tuning_options
+    
+    

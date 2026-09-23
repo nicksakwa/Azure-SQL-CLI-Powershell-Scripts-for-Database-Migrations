dc1dd9b191dad723dfcccac1c14816b1c6e3b998
nickspartan01 <nsakwwa@gmail.com>
2026-09-23 11:14:49 +0300

    options to optimize cityname

--- OptimizeQueryPerformance/queryhints.sql
+++ OptimizeQueryPerformance/queryhints.sql
@@ -1,2 +1,8 @@
 DECLARE @city_name nvarchar(30) ='Acheim',
         @postal_code nvarchar(15) = 86171;
+
+SELECT * 
+FROM Person.Address
+WHERE city = @city_name
+    AND postal_code = @postal_code
+OPTION (OPTIMIZE FOR (@city_name = 'seattle'));
